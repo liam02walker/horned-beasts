8a881e8327c0b02cef0b73d5fa21c798ea9dafad
liam02walker <17liamwalker02@gmail.com>
2023-10-24 14:46:23 +0100

    state done lab-02

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+Number and name of feature: #1: Display Images
+
+Estimate of time needed to complete: 1 Hour
+
+Start time: 14:15
+
+Finish time: 14:35
+
+Actual time needed to complete: 20 Minutes
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export default function Gallery(props) {
+  const [likes, setLikes] = useState(0);
+
+  function likeSetter() {
+    setLikes(likes + 1);
+  }
   return (
     <div className="galleryImages">
       <h2 className="galleryTitle">{props.title}</h2>
-      <img src={props.imgURL} alt={props.desc} className="galleryImage" />
+      <h3>FAVOURITES: {likes}</h3>
+      <div className="imageContainer">
+        <img src={props.imgURL} alt={props.desc} className="galleryImage" />
+        <span className="likeButton" onClick={likeSetter}>
+          ❤️
+        </span>
+      </div>
       <p className="galleryDesc">{props.desc}</p>
     </div>
   );
